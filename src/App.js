c02e1a750908fe6d153d0b124150be631fa66061
Guilherme Eiras <guialbertoeiras@hotmail.com>
2020-05-20 15:05:37 -0300

    feat(menu): add time selection

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Container } from 'semantic-ui-react'
+import Menu from './Menu';
 
 export default function App() {
   return <Container fluid>
+    <Menu onPlay={(time) => console.log(time)} />
   </Container>
 }
---
+++ src/Menu.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './assets/banner.png';
+
+import { Card, Label, Image, Button } from 'semantic-ui-react'
+
+export default function Menu({ onPlay }) {
+  const [time, setTime] = React.useState(null);
+
+  return <div className="Menu">
+    <Image src={logo} fluid />
+    <Card fluid>
+      <Card.Content>
+        <Card.Header>Duração do dia</Card.Header>
+      </Card.Content>
+      <Card.Content>
+        <Label as='a' onClick={() => setTime(300)} color={time === 300 ? 'blue' : null}>05:00</Label>
+        <Label as='a' onClick={() => setTime(450)} color={time === 450 ? 'blue' : null}>07:30</Label>
+        <Label as='a' onClick={() => setTime(600)} color={time === 600 ? 'blue' : null}>10:00</Label>
+        <Label as='a' onClick={() => setTime(720)} color={time === 720 ? 'blue' : null}>12:00</Label>
+        <Label as='a' onClick={() => setTime(900)} color={time === 900 ? 'blue' : null}>15:00</Label>
+        <Label as='a' onClick={() => setTime(0)} color={time === 0 ? 'blue' : null}>Ilimitado</Label>
+      </Card.Content>
+      <Card.Content>
+        <Button basic color='green' fluid onClick={() => onPlay(time)}>Jogar</Button>
+      </Card.Content>
+    </Card>
+  </div>
+}

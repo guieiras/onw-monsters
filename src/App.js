2267406eefbe9b2b4d78ff7c3941f8e4fd0a5efd
Guilherme Eiras <guialbertoeiras@hotmail.com>
2020-05-20 22:45:01 -0300

    feat(menu): add interaction

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import { Container } from 'semantic-ui-react'
 import Menu from './Menu';
 
 export default function App() {
+  const [clock, setClock] = React.useState(null);
+  const audioRef = React.useRef();
+  const prepareAndSetClock = (time) => {
+    audioRef.current.src = null;
+    audioRef.current.play();
+    setClock(time);
+  }
+
   return <Container fluid>
-    <Menu onPlay={(time) => console.log(time)} />
+    { clock === null && <Menu onPlay={prepareAndSetClock} /> }
+    <audio ref={audioRef} />
   </Container>
 }
